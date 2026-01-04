3b43f2123e5260fc3ff1ecca87e25b7a269ab505
PaoloMucedero <p.mucedero@gmail.com>
2026-01-04 12:30:22 +0100

    Aggiunte righe di codice da inserire in presentazione

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+element.classList: //Ritorna nomi classi CSS associate all'elemento
+
+element.style: //Legge e può modificare singole proprietà CSS
+
+element.addEventListener(): //"Ascolta" un evento ed esegue una funzione
+
+
+
+element.innerText: //Ritorna il testo contenuto in un elemento
+element.innerText = text // Imposta come contenuto la parola "text"
+
+
+element.innerHTML: //Ritorna HTML dell'elemento, se ne possono anche creare di nuovi
+element.innerHTML = text // Imposta come contenuto del tag HTML la parola "text"
+
